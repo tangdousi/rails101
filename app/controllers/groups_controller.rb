2af3bd5fa258d90794943ec59f1929fc03f85e2e
summer <liubihan56560@gmail.com>
2018-05-18 09:58:13 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,46 +1,50 @@
 class GroupsController < ApplicationController
+
   def index
     @groups = Group.all
   end
 
-def show
-  @group = Group.find(params[:id])
-end
+  def new
+    @group = Group.new
+  end
 
-def edit
-  @group = Group.find(params[:id])
-end
+  def show
+    @group = Group.find(params[:id])
+  end
 
-def new
-  @group = Group.new
-end
+  def edit
+    @group = Group.find(params[:id])
+  end
 
-def create
-  @group = Group.new(group_params)
-  @group.save
+  def create
+    @group = Group.new(group_params)
 
-  redirect_to groups_path
-end
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
 
-def update
-  @group = Group.find(params[:id])
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: 'Update Success'
+    else
+      render :edit
+    end
+  end
 
-  @group.update(group_params)
+  def destroy
+    @group = Group.find(params[:id])
 
-  redirect_to groups_path, notice: "Update Success"
-end
+    @group.destroy
+    redirect_to groups_path, alert: 'Group deleted'
+  end
 
-def destroy
-  @group = Group.find(params[:id])
-  @group.destroy
-  flash[:alert] = "Group deleted"
-  redirect_to groups_path
-end
+  private
 
-private
-
-def group_params
-  params.require(:group).permit(:title, :description)
-end
-
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
